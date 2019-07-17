82f0a14fbb67c58d59b0d72c8993040ea730c2dd
Daniel <danielgatis@gmail.com>
2019-07-17 12:55:37 -0300

    proxy

--- examples/demo.js
+++ examples/demo.js
@@ -1,25 +1,30 @@
 const puppeteer = require('puppeteer')
 const solve = require('../index.js')
 
+// to avoid IP bans get a residential proxy
+// like: luminati.io
+
+PROXY_SERVER = '127.0.0.1:9876' // YOUR PROXY HERE!
+
 async function run () {
   const browser1 = await puppeteer.launch({
     headless: false,
-    args: ['--window-size=640,800', '--window-position=0,0']
+    args: ['--window-size=640,800', '--window-position=0,0', `--proxy-server=${PROXY_SERVER}`]
   })
 
   const browser2 = await puppeteer.launch({
     headless: false,
-    args: ['--window-size=640,800', '--window-position=640,0']
+    args: ['--window-size=640,800', '--window-position=640,0', `--proxy-server=${PROXY_SERVER}`]
   })
 
   const browser3 = await puppeteer.launch({
     headless: false,
-    args: ['--window-size=640,800', '--window-position=0,800']
+    args: ['--window-size=640,800', '--window-position=0,800', `--proxy-server=${PROXY_SERVER}`]
   })
 
   const browser4 = await puppeteer.launch({
     headless: false,
-    args: ['--window-size=640,800', '--window-position=640,800']
+    args: ['--window-size=640,800', '--window-position=640,800', `--proxy-server=${PROXY_SERVER}`]
   })
 
   const page1 = await browser1.newPage()
